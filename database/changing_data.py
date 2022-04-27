461832c0cff34ce2632f9b11ad10b767eff222dc
Aleksandr K <akobelyov@gmail.com>
2022-04-27 15:06:53 +0500

    рефакторинг модуля с бд

---
+++ database/changing_data.py
@@ -0,0 +1,97 @@
+from conf import type_product, type_menu
+from database.sqlite_db import CUR, BASE
+from database import data_request
+
+
+# Изменение цены, процента потерь и имени в таблице products
+def change_name_price_percent_in_table(name_product, price, percent):
+    CUR.execute(f'UPDATE products SET price = {price}, percent_loss = {percent} '
+                f'WHERE name_product = "{name_product}";')
+    BASE.commit()
+
+
+# Добавление строки в таблицу products
+def adding_row_to_table(category, name, price, percent_off):
+    CUR.execute(f'INSERT INTO products (type_product, name_product, price, percent_loss) VALUES '
+                f'("{type_product[category]}", "{name}", {price}, {percent_off});')
+    BASE.commit()
+
+
+# Удаление строки из products
+def deleting_row(name):
+    CUR.execute(f'DELETE FROM products WHERE name_product = "{name}";')
+    BASE.commit()
+
+
+# Удаление таблицы и строки их menu
+def deleting_table_and_row(name):
+    CUR.execute(f'DROP TABLE "{name}";')
+    CUR.execute(f'DELETE FROM menu WHERE name = "{name}";')
+    BASE.commit()
+
+
+# Создание таблицы с блюдом
+def creating_dish_table(name_dish):
+    CUR.execute(f'CREATE TABLE "{name_dish}" (products TEXT, brutto REAL, netto REAL, price_kg REAL, summ REAL);')
+    BASE.commit()
+
+
+# Добавление блюд в таблицу
+def add_dish_to_table(name_dishes, name_product, brutto_product, netto_product, price_kg, summ):
+    CUR.execute(f'INSERT INTO "{name_dishes}" (products, brutto, netto, price_kg, summ) '
+                f'VALUES ("{name_product}", {brutto_product}, {netto_product}, {price_kg}, {summ});')
+    BASE.commit()
+
+
+# Добавление блюда в таблицу menu
+def add_dish_to_table_menu(category, name, price):
+    CUR.execute(f'INSERT INTO menu (type_menu, name, price) VALUES ("{type_menu[category]}", "{name}", {price});')
+    BASE.commit()
+
+
+# обновление цен и суммы в products
+def update_price_kg_sum_in_products():
+    names_from_menu = data_request.get_name_from_menu()
+    names_prices = data_request.get_names_prices_from_product()
+
+    for name in names_from_menu:
+        names_product = data_request.get_product_from_dish(name)
+
+        for name_price in names_prices:
+
+            try:
+                if name_price[0] in names_product:
+                    price = float(name_price[1])
+                    brutto = data_request.get_brutto(name, name_price[0])
+                    brutto = float(brutto)
+                    summar = round(price * brutto / 1000)
+                    CUR.execute(
+                        f'UPDATE "{name}" SET price_kg = {price}, summ = {summar} WHERE products = "{name_price[0]}";')
+                    BASE.commit()
+
+            except Exception:
+                continue
+
+
+# Обновление цены в menu
+def update_price_in_menu():
+    names_from_menu = data_request.get_name_from_menu()
+
+    for name in names_from_menu:
+        try:
+
+            summar = data_request.get_sum(name)
+            netto = data_request.get_netto(name)
+            netto = float(netto)
+
+            price = round((float(summar) * 1000) / netto)
+            CUR.execute(f'UPDATE menu SET price = {price} WHERE name = "{name}";')
+            BASE.commit()
+        except Exception:
+            continue
+
+
+# обновление цен в таблице menu
+def update_price():
+    update_price_kg_sum_in_products()
+    update_price_in_menu()
---
+++ database/data_request.py
@@ -0,0 +1,106 @@
+import re
+from conf import type_product
+from database.sqlite_db import CUR, PATTERN
+
+
+# Выбор из таблицы название продукта, цену и процент потерь
+def get_name_price_from_table_products(category):
+    CUR.execute(f'SELECT name_product, price, percent_loss FROM products '
+                f'WHERE type_product = "{type_product[category]}";')
+    rows_data = CUR.fetchall()
+    name_price_percent_products = [re.sub(PATTERN, '', str(data)) for data in rows_data]
+    return name_price_percent_products
+
+
+# Выбор названия продуктов для создания кнопок клавиатуры
+def get_names_for_keyboard(category):
+    CUR.execute(f'SELECT name_product FROM products WHERE type_product = "{type_product[category]}";')
+    rows_data = CUR.fetchall()
+    names_product = [re.sub(PATTERN, '', str(data)) for data in rows_data]
+    return names_product
+
+
+# Получние процента потерь и цены
+def get_percent_price(name_prod):
+    percent = CUR.execute(f'SELECT percent_loss FROM products WHERE name_product = "{name_prod}";').fetchall()
+    price = CUR.execute(f'SELECT price FROM products WHERE name_product = "{name_prod}";').fetchall()
+    percent = re.sub(PATTERN, '', str(percent))
+    price = re.sub(PATTERN, '', str(price))
+    return percent, price
+
+
+# Получение суммы из столбца summ
+def get_sum(name_dish):
+    CUR.execute(f'SELECT SUM(summ) FROM "{name_dish}";')
+    summar = CUR.fetchall()
+    summar = re.sub(PATTERN, '', str(summar))
+    return summar
+
+
+# Получение нетто
+def get_netto(name_dish):
+    CUR.execute(f'SELECT SUM(netto) FROM "{name_dish}";')
+    netto = CUR.fetchall()
+    netto = re.sub(PATTERN, '', str(netto))
+    return netto
+
+
+# Получени процента потерь
+def get_percent_loss(name_product):
+    CUR.execute(f'SELECT percent_loss FROM products WHERE name_product = "{name_product}";')
+    percent_loss = CUR.fetchall()
+    percent_loss = re.sub(PATTERN, '', str(percent_loss))
+    return percent_loss
+
+
+# Получение имён из таблицы menu
+def get_name_menu(type_name):
+    CUR.execute(f"SELECT name FROM menu WHERE type_menu = '{type_name}';")
+    rows_data = CUR.fetchall()
+    names_menu = [re.sub(PATTERN, '', str(data)) for data in rows_data]
+    return names_menu
+
+
+# Получение имён и цен из menu
+def get_name_price_menu(type_name):
+    CUR.execute(f"SELECT name, price FROM menu WHERE type_menu = '{type_name}';")
+    rows_data = CUR.fetchall()
+    names_prices_products = [re.sub(PATTERN, '', str(data)) for data in rows_data]
+    return names_prices_products
+
+
+# Получение блюд из меню
+def get_name_from_menu():
+    CUR.execute('SELECT name FROM menu;')
+    rows_data = CUR.fetchall()
+    names_from_menu = [re.sub(PATTERN, '', str(data)) for data in rows_data]
+    return names_from_menu
+
+
+# Получение имён и цен из products
+def get_names_prices_from_product():
+    names_product = CUR.execute('SELECT name_product FROM products;').fetchall()
+    prices_product = CUR.execute('SELECT price FROM products;').fetchall()
+    names_from_product = [re.sub(PATTERN, '', str(data)) for data in names_product]
+    prices_from_product = [re.sub(PATTERN, '', str(data)) for data in prices_product]
+    names_prices = zip(names_from_product, prices_from_product)
+    return list(names_prices)
+
+
+# Получени состава продуктов из блюда
+def get_product_from_dish(dish):
+    try:
+        CUR.execute(f'SELECT products FROM "{dish}";')
+        name_products = CUR.fetchall()
+        name_products = re.sub(PATTERN, '', str(name_products))
+        return name_products
+    except Exception:
+        pass
+
+
+# Получение брутто
+def get_brutto(name, product):
+    CUR.execute(f'SELECT brutto FROM "{name}" WHERE products = "{product}";')
+    brutto = CUR.fetchall()
+    brutto = re.sub(PATTERN, '', str(brutto))
+    return brutto
